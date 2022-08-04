9545a5d9e9238b41506d87ba7f7ec34f0ffa8393
MATEUS SANTOS BISPO <72230496+mateusb23@users.noreply.github.com>
2022-08-04 20:16:41 -0300

    POO
    estudos relacionados à aula 05

---
+++ Aula05/src/aula05/Aula05.java
@@ -0,0 +1,13 @@
+package aula05;
+
+public class Aula05 {
+
+	public static void main(String[] args) {
+		Caneta c1 = new Caneta("FABER CASTELL", "Vermelha", 0.7f);
+		c1.status();
+		
+		Caneta c2 = new Caneta("BIC", "Preta", 0.5f);
+		c2.status();
+	}
+
+}
