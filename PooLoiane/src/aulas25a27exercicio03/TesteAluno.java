a1f43ccc1923ac04433ca6e3576b307ec4537960
MATEUS SANTOS BISPO <72230496+mateusb23@users.noreply.github.com>
2022-08-29 21:12:41 -0300

    Update TesteAluno.java

--- PooLoiane/src/aulas25a27exercicio03/TesteAluno.java
+++ PooLoiane/src/aulas25a27exercicio03/TesteAluno.java
@@ -4,8 +4,11 @@ public class TesteAluno {
 
 	public static void main(String[] args) {
 		
+		Aluno pessoa1 = new Aluno();
+		pessoa1.nomeDisciplina();
+		pessoa1.notaDisciplina();
+		pessoa1.statusAluno();
 		
-
 	}
 
 }
