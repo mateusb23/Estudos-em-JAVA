b3c438de196f99f4987f17dd24cdf7ca68797a97
MATEUS SANTOS BISPO <72230496+mateusb23@users.noreply.github.com>
2022-09-01 21:30:24 -0300

    Update Aluno.java

--- PooLoiane/src/aulas25a27exercicio03/Aluno.java
+++ PooLoiane/src/aulas25a27exercicio03/Aluno.java
@@ -3,54 +3,67 @@ package aulas25a27exercicio03;
 import java.util.Scanner;
 
 public class Aluno {
+    Scanner teclado = new Scanner(System.in);
 
-	String nome;
-	int matricula;
-	String curso;
-	String[] disciplinas;
-	float[] notas;
-	boolean aprovacao;
-	
-	void nomeAluno(String nomeEstudante) {
-		this.nome = nomeEstudante;
-	}
-	
-	void numeroMatricula(int numeroDeMatricula) {
-		this.matricula = numeroDeMatricula;
-	}
-	
-	void nomeCurso(String nomeDoCurso) {
-		this.curso = nomeDoCurso;
-	}
-	
-	Scanner teclado = new Scanner(System.in);
-	void nomeDisciplina() {
-		disciplinas = new String[3];
-		for (int i = 0 ; i < 3 ; i++) {
-			System.out.println("informe a disciplina " + i + ": ");
-			disciplinas[i] = teclado.next();
-		}
-	}
-	
-	void notaDisciplina() {
-		notas = new float[3];
-		for (int i = 0 ; i < 3 ; i++) {
-			System.out.println("Informe a nota " + i + ": ");
-			notas[i] = teclado.nextFloat();
-		}
-	}
-	
-	void statusAluno() {
-		
-		System.out.println("As notas e disciplinas do aluno " + nome + " são: ");
-		for (int i = 0 ; i < 3 ; i++) {
-			System.out.println(notas[i]);
-			if (notas[i] >= 7) {
-				System.out.println("Aprovado na matéria " + disciplinas[i]);
-			} else {
-				System.out.println("Reprovado na matéria " + disciplinas[i]);
-			}
-		}
-		
-	}
+    // ATRIBUTPS
+    String nome;
+    int matricula;
+    String curso;
+    String[] disciplinas;
+    float[] notas;
+
+    // MÉTODOS
+
+    // MÉTODOS COM PARÂMETRO E SEM RETORNO
+    void informarNome(String nome) {
+        this.nome = nome;
+    }
+
+    void numeroMatricula(int numero) {
+        this.matricula = numero;
+    }
+
+    // MÉTODOS COM RETORNO --> RESOLVI NÃO COLOCAR UM COM RETORNO, POIS AS OPERAÇÕES SÃO MUITO SIMPLES
+
+    // MÉTODOS SEM PARÂMETRO
+    void informarCurso() {
+        System.out.println("Informe o seu curso: ");
+        curso = teclado.nextLine();
+    }
+
+    void informarDisciplinasNotas() {
+        disciplinas = new String[3];
+        notas = new float[3];
+
+        for (int i = 0 ; i < 3 ; i++) {
+            System.out.println("Informe a sua " + (i+1) + " disciplina: ");
+            disciplinas[i] = teclado.next();
+            for (int j = i ; j <= i ; j++) {
+                System.out.println("Diga a nota de " + disciplinas[i] + ": ");
+                notas[i] = teclado.nextFloat();
+                while (notas[i] < 0) {
+                    System.out.println("Digite uma nota valida para " + disciplinas[i] + ": ");
+                    notas[i] = teclado.nextFloat();
+                }
+
+            }
+
+        }
+
+    }
+
+    void mostrarStatus() {
+
+        System.out.println("As notas e disciplinas do aluno " + nome + " são: ");
+        for (int i = 0 ; i < 3 ; i++) {
+            System.out.println(notas[i]);
+            if (notas[i] >= 7) {
+                System.out.println("Aprovado na matéria " + disciplinas[i]);
+            } else {
+                System.out.println("Reprovado na matéria " + disciplinas[i]);
+            }
+        }
+
+    }
+
 }
