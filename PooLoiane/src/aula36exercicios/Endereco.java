79a74a7a68e55dc2ba44087d51828a14e59fb47d
MATEUS SANTOS BISPO <72230496+mateusb23@users.noreply.github.com>
2022-09-08 20:12:39 -0300

    Add files via upload

---
+++ PooLoiane/src/aula36exercicios/Endereco.java
@@ -0,0 +1,62 @@
+package com.loiane.cursojava.aula36exercicios;
+
+public class Endereco {
+
+	private String rua;
+	private String numeroCasa;
+	private String bairro;
+	private String cidade;
+	private String estado;
+	private String pais;
+	
+	public Endereco() { }
+
+	public String getRua() {
+		return rua;
+	}
+
+	public void setRua(String rua) {
+		this.rua = rua;
+	}
+
+	public String getNumeroCasa() {
+		return numeroCasa;
+	}
+
+	public void setNumeroCasa(String numeroCasa) {
+		this.numeroCasa = numeroCasa;
+	}
+
+	public String getBairro() {
+		return bairro;
+	}
+
+	public void setBairro(String bairro) {
+		this.bairro = bairro;
+	}
+
+	public String getCidade() {
+		return cidade;
+	}
+
+	public void setCidade(String cidade) {
+		this.cidade = cidade;
+	}
+
+	public String getEstado() {
+		return estado;
+	}
+
+	public void setEstado(String estado) {
+		this.estado = estado;
+	}
+
+	public String getPais() {
+		return pais;
+	}
+
+	public void setPais(String pais) {
+		this.pais = pais;
+	}
+	
+}
