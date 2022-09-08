18a4c4ebb72f861f7466e0092a4a1ba885618387
MATEUS SANTOS BISPO <72230496+mateusb23@users.noreply.github.com>
2022-09-08 20:09:57 -0300

    Add files via upload

---
+++ aula36exercicios/Agenda.java
@@ -0,0 +1,17 @@
+package com.loiane.cursojava.aula36exercicios;
+
+public class Agenda {
+
+	private Contato contato;
+
+	public Agenda() { }
+
+	public Contato getContato() {
+		return contato;
+	}
+
+	public void setContato(Contato contato) {
+		this.contato = contato;
+	}
+	
+}
