cbed5872f78096aa14278ef7e644516ee1cd629f
MATEUS SANTOS BISPO <72230496+mateusb23@users.noreply.github.com>
2022-09-14 19:16:05 -0300

    Update Contas.java

--- ProjetoFinalModulo01Fuctura/src/ProjetoFinalBancoFuctura/Contas.java
+++ ProjetoFinalModulo01Fuctura/src/ProjetoFinalBancoFuctura/Contas.java
@@ -2,61 +2,61 @@ package ProjetoFinalBancoFuctura;
 
 public abstract class Contas {
 
-    private String numConta;
-    private String nome;
-    private String cpf;
-    private double saldoTotal;
-    private int senha;
+	private String numConta;
+	private String nome;
+	private String cpf;
+	private double saldoTotal;
+	private int senha;
 
-    public Contas() { }
+	public Contas() { }
 
-    public String getNumConta() {
-        return numConta;
-    }
+	public String getNumConta() {
+		return numConta;
+	}
 
-    public void setNumConta(String numConta) {
-        this.numConta = numConta;
-    }
+	public void setNumConta(String numConta) {
+		this.numConta = numConta;
+	}
 
-    public String getNome() {
-        return nome;
-    }
+	public String getNome() {
+		return nome;
+	}
 
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
 
-    public String getCpf() {
-        return cpf;
-    }
+	public String getCpf() {
+		return cpf;
+	}
 
-    public void setCpf(String cpf) {
-        this.cpf = cpf;
-    }
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
 
-    public double getSaldoTotal() {
-        return saldoTotal;
-    }
+	public double getSaldoTotal() {
+		return saldoTotal;
+	}
 
-    public void setSaldoTotal(double saldo) {
-        this.saldoTotal = saldo;
-    }
+	public void setSaldoTotal(double saldo) {
+		this.saldoTotal = saldo;
+	}
 
-    public int getSenha() {
-        return senha;
-    }
+	public int getSenha() {
+		return senha;
+	}
 
-    public void setSenha(int senha) {
-        this.senha = senha;
-    }
+	public void setSenha(int senha) {
+		this.senha = senha;
+	}
 
-    public void depositar(double valorDepositado) {
-        ContaCorrente.setSaldoCorrente(ContaCorrente.getSaldoCorrente() + valorDepositado);
-    }
+	public void depositar(double valorDepositado) {
+		ContaCorrente.setSaldoCorrente(ContaCorrente.getSaldoCorrente() + valorDepositado);
+	}
 
-    public void exibirSaldoTotal() {
-        setSaldoTotal(ContaCorrente.getSaldoCorrente() + ContaPoupanca.getSaldoPoupanca());
-        System.out.println("Saldo total = " + getSaldoTotal());
-    }
+	public void exibirSaldoTotal() {
+		setSaldoTotal(ContaCorrente.getSaldoCorrente() + ContaPoupanca.getSaldoPoupanca());
+		System.out.println("Saldo total = " + getSaldoTotal());
+	}
 
 }
