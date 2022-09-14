9d4f7e8920b602520bc2d1d87ad36e8b5c2fba8a
MATEUS SANTOS BISPO <72230496+mateusb23@users.noreply.github.com>
2022-09-14 19:17:27 -0300

    Update ContaPoupanca.java

--- ProjetoFinalModulo01Fuctura/src/ProjetoFinalBancoFuctura/ContaPoupanca.java
+++ ProjetoFinalModulo01Fuctura/src/ProjetoFinalBancoFuctura/ContaPoupanca.java
@@ -2,29 +2,28 @@ package ProjetoFinalBancoFuctura;
 
 public class ContaPoupanca extends ContaCorrente {
 
-    private static double saldoPoupanca;
+	private static double saldoPoupanca;
 
-    public ContaPoupanca() {
-        ContaPoupanca.saldoPoupanca = getSaldoTotal() - getSaldoCorrente();
-    }
+	public ContaPoupanca() {
+		ContaPoupanca.saldoPoupanca = getSaldoTotal() - getSaldoCorrente();
+	}
 
-    public static double getSaldoPoupanca() {
-        return ContaPoupanca.saldoPoupanca;
-    }
+	public static double getSaldoPoupanca() {
+		return ContaPoupanca.saldoPoupanca;
+	}
 
-    public static void setSaldoPoupanca(double saldoContaPoupanca) {
-        ContaPoupanca.saldoPoupanca = saldoContaPoupanca;
-    }
+	public static void setSaldoPoupanca(double saldoContaPoupanca) {
+		ContaPoupanca.saldoPoupanca = saldoContaPoupanca;
+	}
 
-    public void resgatar(double valorResgatado) {
-        setSaldoCorrente(getSaldoCorrente() + valorResgatado);
-        setSaldoPoupanca(getSaldoPoupanca() - valorResgatado);
-        System.out.println("Valor resgatado da poupança = " + valorResgatado);
-    }
+	public void resgatar(double valorResgatado) {
+		setSaldoCorrente(getSaldoCorrente() + valorResgatado);
+		setSaldoPoupanca(getSaldoPoupanca() - valorResgatado);
+		System.out.println("Valor resgatado da poupança = " + valorResgatado);
+	}
 
-    public void mostrarSaldoPoupanca() {
-        System.out.println("Saldo da sua poupança = " + getSaldoPoupanca());
-    }
+	public void mostrarSaldoPoupanca() {
+		System.out.println("Saldo da sua poupança = " + getSaldoPoupanca());
+	}
 
 }
-
