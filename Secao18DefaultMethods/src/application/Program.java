9e14ce83d091845d112d6cbe990d7aa565a41cc3
MATEUS SANTOS BISPO <72230496+mateusb23@users.noreply.github.com>
2022-10-31 00:04:55 -0300

    Add files via upload

---
+++ Secao18DefaultMethods/src/application/Program.java
@@ -0,0 +1,20 @@
+package application;
+
+import java.util.Scanner;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		Scanner teclado = new Scanner(System.in);
+		
+		System.out.print("Amount: ");
+		double amount = teclado.nextDouble();
+		System.out.print("Months: ");
+		int months = teclado.nextInt();
+		
+		Bra
+		
+	}
+	
+}
